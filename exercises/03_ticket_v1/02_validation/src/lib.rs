8054a8d7547e1110234390f63831182584bb104f
stevenprimeaux <stevenprimeaux@gmail.com>
2025-01-10 15:14:04 -0500

    Start Chapter 3.

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -5,20 +5,25 @@ struct Ticket {
 }
 
 impl Ticket {
-    // TODO: implement the `new` function.
-    //  The following requirements should be met:
-    //   - Only `To-Do`, `In Progress`, and `Done` statuses are allowed.
-    //   - The `title` and `description` fields should not be empty.
-    //   - the `title` should be at most 50 bytes long.
-    //   - the `description` should be at most 500 bytes long.
-    //  The method should panic if any of the requirements are not met.
-    //  You can find the needed panic messages in the tests.
-    //
-    // You'll have to use what you learned in the previous exercises,
-    // as well as some `String` methods. Use the documentation of Rust's standard library
-    // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        if title.is_empty() {
+            panic!("Title cannot be empty")
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes")
+        }
+
+        if description.is_empty() {
+            panic!("Description cannot be empty")
+        }
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes")
+        }
+
+        if !["To-Do", "In Progress", "Done"].contains(&&status[..]) {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed")
+        }
+
         Self {
             title,
             description,
